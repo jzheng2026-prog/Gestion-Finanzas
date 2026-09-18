c4854a8e8bbf42265cba019593bc0d42db7fec88
jzheng <jzhengdaw2026@gmail.com>
2026-09-18 21:54:19 +0200

    traemos el middleware de vuelta

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  return NextResponse.next()
   const supabaseResponse = NextResponse.next({ request })
 
   const supabase = createServerClient(
